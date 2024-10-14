965f415acc01964108b1d0c3e019c5672f0141fd
hamza <hamzajutt552@gmail.com>
2024-10-14 18:49:18 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+tailwind.config = {
+  darkMode: 'class', // Enable dark mode using a CSS class
+}
+
